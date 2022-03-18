f300b7cf13432d5fe5b943b411a1c79e6c63e85a
Sammwy <sammwy.dev@gmail.com>
2022-03-17 23:42:18 -0300

    Added tphere

--- src/main/java/com/madkaos/core/commands/admin/TeleportPosCommand.java
+++ src/main/java/com/madkaos/core/commands/admin/TeleportPosCommand.java
@@ -4,6 +4,7 @@ import com.madkaos.core.commands.Argument;
 import com.madkaos.core.commands.Command;
 import com.madkaos.core.commands.CommandContext;
 import com.madkaos.core.commands.CommandListener;
+import com.madkaos.core.player.MadPlayer;
 
 @Command(name = "tpos", permission = "core.tpos", minArguments = 3, arguments = {
         Argument.INT,
@@ -13,10 +14,18 @@ import com.madkaos.core.commands.CommandListener;
 public class TeleportPosCommand extends CommandListener {
     @Override
     protected void onExecuteByPlayer(CommandContext ctx) {
+        MadPlayer player = ctx.getPlayer();
+
         int x = ctx.getArguments().getInt(0);
         int y = ctx.getArguments().getInt(1);
         int z = ctx.getArguments().getInt(2);
 
-        ctx.getPlayer().teleport(x, y, z);
+        player.teleport(x, y, z);
+        player.sendMessage(
+            player.getI18nMessage("tp.teleported")
+                .replace("{x}", x + "")
+                .replace("{y}", y + "")
+                .replace("{z}", z + "")
+        );
     }
 }
