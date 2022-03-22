386df245f337fe427c843d9107149d649538c6b1
Sammwy <sammwy.dev@gmail.com>
2022-03-22 17:38:06 -0300

    some changes

--- src/main/java/com/madkaos/core/utils/ProxyUtils.java
+++ src/main/java/com/madkaos/core/utils/ProxyUtils.java
@@ -6,21 +6,19 @@ import java.io.DataOutputStream;
 import com.madkaos.core.player.MadPlayer;
 
 public class ProxyUtils {
-    public static void sendPlayerToServer(MadPlayer player, String server) {
-        try {
-          ByteArrayOutputStream b = new ByteArrayOutputStream();
-          DataOutputStream out = new DataOutputStream(b);
-          out.writeUTF("Connect");
-          out.writeUTF(server);
-          player.sendPluginMessage("BungeeCord", b.toByteArray());
-          b.close();
-          out.close();
-        }
-        catch (Exception e) {
-          player.sendMessage(
-              player.getI18nMessage("server.unknown-error")
-                .replace("{server}", server)  
-          );
-        }
-      }
+  public static void sendPlayerToServer(MadPlayer player, String server) {
+    try {
+      ByteArrayOutputStream b = new ByteArrayOutputStream();
+      DataOutputStream out = new DataOutputStream(b);
+      out.writeUTF("Connect");
+      out.writeUTF(server);
+      player.sendPluginMessage("BungeeCord", b.toByteArray());
+      b.close();
+      out.close();
+    } catch (Exception e) {
+      player.sendMessage(
+          player.getI18nMessage("server.unknown-error")
+              .replace("{server}", server));
+    }
+  }
 }
